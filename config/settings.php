8e0c89c91162ea6e874b5e3e1ba46b6bc01924e5
Eser Sarıyar <35234218+esersariyar@users.noreply.github.com>
2022-06-05 03:43:51 +0300

    Create settings.php
    Here you can adjust the bot settings.

---
+++ config/settings.php
@@ -0,0 +1,32 @@
+<?php
+/*
+
+Bu yazılım Eser Sarıyar tarafından geliştirilmiştir. Yazılımın çeşitli alanlarının düzenlenmesi serbesttir.
+
+- Copyright metinlerinin silinmesi yasaktır. (Örneğin: Eser Sarıyar metni gibi)
+- Yazılımın satılması kesinlikle yasaktır.
+- Yazılım GitHub üzerinde paylaşılacaksa, Eser Sarıyar'ın paylaşmış olduğu repositorie üzerinden fork aracılığı ile paylaşılabilir.
+
+
+Yazılımın geliştirilme tarihi: 04.06.2022
+*/
+
+
+// BU BÖLÜM ÜZERİNDEN BOT AYARLARINI YAPABİLİRSİNİZ //
+
+$bot_name = "Selin"; // Buradan botun adını değiştirebilirsiniz.
+$command_p = "!"; // Buradan komut parametresini değiştirebilirsiniz. (Örneğin: !yeniveri yerine +yeniveri yapabilirsiniz.)
+$site_title = "Selin - Türk Bot"; // Buradan site başlığını güncelleyebilirsiniz.
+$site_favicon = "https://cdn.meetweb.me/icon.png"; // Site faviconunu buradan güncelleyebilirsiniz.
+$bot_pp = "https://i.hizliresim.com/lfsu5nn.jpg"; // Buradan botun profil resmini güncelleyebilirsiniz.
+$visitor_name = "Ziyaretçi"; // Buradan bot ile sohbet eden kişinin adını belirleyebilirsiniz.
+
+
+// VERİTABANI ALANI //
+
+$servername = "localhost"; // Bu alana sunucu adını yazınız. Genelde localhost kullanılır.
+$dbname = ""; // Bu alana veritabanı adını yazınız.
+$username = ""; // Bu alana veritabanı kullanıcı adınızı yazınız.
+$password = ""; // Bu alana veritabanı şifrenizi yazınız.
+
+?>
